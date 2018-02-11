42e2a2dfa297980fe7bad282f3952cd1de293b27
hugo boiton <hugo_boiton@hotmail.com>
2018-02-10 22:02:38 -0600

    inicio de primer proyecto

---
+++ src/Generar/AnalizadorCCSS/GenerarAnalizadorCCSS.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Generar.AnalizadorCCSS;
+
+/**
+ *
+ * @author Hugo
+ */
+public class GenerarAnalizadorCCSS {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/Generar/AnalizadorCJS/GenerarAnalizadorCJS.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Generar.AnalizadorCJS;
+
+/**
+ *
+ * @author Hugo
+ */
+public class GenerarAnalizadorCJS {
+    public static void main(String[] args) {
+        
+    }
+}
