f27e9572fd995f973d07ae7bc30446526274cb75
hugo boiton <hugo_boiton@hotmail.com>
2018-02-21 21:47:39 -0600

    qwerwsw safsasdasasdasdfasd

---
+++ src/Analizador/CCSS/ConstantesCSS.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Analizador.CCSS;
+
+import com.sun.xml.internal.ws.api.ha.StickyFeature;
+import javax.print.DocFlavor;
+
+/**
+ *
+ * @author Hugo
+ */
+public class ConstantesCSS {
+    
+    public static final String error = "error";
+    
+    public static final String inicio = "inicio";
+    public static final String encabezado = "encabezado";
+    public static final String compencabezado ="compencabezado";
+    public static final String cuerpo = "cuerpocss";
+    public static final String compcuerpo="compcuerpo";
+    public static final String elemento = "elementoscuerpo";
+    public static final String id = "id";
+    public static final String grupo = "grupo";
+    
+    public static final String alineado = "alineado";
+    public static final String texto = "texto";
+    public static final String formato = "formato";
+    public static final String letra = "letra";
+    public static final String tamtex = "tamtex";
+    public static final String fondoelemento ="fondoelemento";
+    public static final String autoredi = "autoredi";
+    public static final String visible = "visible";
+    public static final String borde = "borde";
+    public static final String opaque = "opaque";
+    public static final String colortext = "colortext";
+    
+    public static final String lopfor = "lopfor";
+    
+    public static final String CAPITALT = "CAPITALT";
+    public static final String MININ = "MININ";
+    public static final String MAYUSC = "MAYUSC";
+    public static final String CURSIVA = "CURSIVA";
+    public static final String NEGRILLA = "NEGRILLA";
+    
+    public static final String JUSTIFICADO = "JUSTICADO";
+    public static final String CENTRADO = "CENTRADO";
+    public static final String DERECHA = "DERECHA";
+    public static final String IZQUIERDA = "IZQUIERDA";
+    
+    public static final String MAS = "MAS";
+    public static final String MENOS ="MENOS";
+    public static final String POR = "POR";
+    public static final String DIVI = "DIVI";
+    
+    public static final String exp="exp";
+    public static final String ECADENA="ecadena";
+    public static final String ENUMERO="enumero";
+    
+    public static final String EVERDADERO = "VERDADERO";
+    public static final String EFALSO = "FALSE";
+    
+    public static final String VERTICAL ="vertical";
+    public static final String HORIZONTAL="horizontal";
+    
+   
+}
---
+++ src/Analizador/CHTML/ConstantesCHTML.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Analizador.CHTML;
+
+/**
+ *
+ * @author Hugo
+ */
+public class ConstantesCHTML {
+    public static final String inicio="inicio";
+    public static final String encabezado="encabezado";
+    public static final String titulo="titulo";
+    public static final String ccss="ccss";
+    public static final String cjs="cjs";
+    public static final String cuerpo ="cuerpo";
+    
+    public static final String opciones ="opciones";
+    public static final String ruta ="ruta";
+    public static final String click = "click";
+    public static final String id = "id";
+    public static final String grupo = "grupo";
+    public static final String ancho = "ancho";
+    public static final String alto ="alto";
+    public static final String alineado = "alineado";
+    public static final String fondo = "fondo";
+    
+    public static final String panel ="panel";
+    public static final String texto ="texto";
+    public static final String imagen ="imagen";
+    public static final String boton = "boton";
+    public static final String enlace ="enlace";
+    
+    public static final String textoa ="textoa";
+    public static final String cajatexto ="cajatexto";
+    
+    public static final String spinner = "spinner";
+    public static final String saltofin = "saltofin";
+    
+    public static final String ECADENA ="ECADENA";
+    public static final String ETEXTO = "ETEXTO";
+    
+    
+    
+}
