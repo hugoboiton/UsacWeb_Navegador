d5a8f90a67122d207486198eaf97f0511f261a2b
hugo boiton <hugo_boiton@hotmail.com>
2018-02-24 01:13:51 -0600

    ast terminados

---
+++ src/Analizador/CJS/ConstantesCJS.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Analizador.CJS;
+
+/**
+ *
+ * @author Hugo
+ */
+public class ConstantesCJS {
+    public static final String error="error";
+    public static final String global="global";
+    public static final String sintipo="sintipo";
+    
+    public static final String texto="texto";
+    public static final String booleano="booleano";
+    public static final String numero="numeros";
+    public static final String fecha="fecha";
+    public static final String fechahora="fechahora";
+    
+    public static final String verdadero="verdadero";
+    public static final String falso ="falso";
+    
+    public static final String eid="eid";
+    
+    public static final String cuerpo="cuerpo";
+    
+    public static final String funcion="funcion";
+    public static final String parametros="parametros";
+    
+    public static final String declararvar="declararvar";
+    public static final String asignarvar="asignarvar";
+    
+    public static final String declararvec="declararvec";
+    public static final String asignarvec="asignarvec";
+    
+    public static final String conteov ="conteov";
+    public static final String atextov="atextov";
+    public static final String valorvec="valorvec";
+    
+    public static final String sen ="sen";
+    
+    public static final String si="si";
+    
+    public static final String seleccionar="seleccionar";
+    public static final String lcaso="lcaso";
+    public static final String caso="caso";
+    public static final String defecto="defecto";
+    
+    public static final String para="para";
+    public static final String mientras="mientras";
+    
+    public static final String detener="detener";
+    public static final String retorno="retorno";
+    
+    public static final String imprimir="imprimir";
+    public static final String mensaje="mensaje";
+    
+    public static final String llamdafuncion="llamadafuncion";
+    
+    public static final String logica="logica";
+    public static final String relacional="relacional";
+    public static final String exp="exp";
+    
+    public static final String or="or";   //1
+    public static final String and="and"; //2
+    public static final String not="not"; //3
+    
+    public static final String igualigual="igualigual"; //1
+    public static final String diferente="diferente";  //2
+    public static final String menor="menor";          //3
+    public static final String menorigual ="menorigual";//4
+    public static final String mayor="mayor";           //5
+    public static final String mayorigual="mayorigual"; //6
+    
+    public static final String mas="mas";        //1
+    public static final String menos="menos";    //2
+    public static final String por="por";        //3
+    public static final String divi="divi";      //4
+    public static final String mod ="mod";      //5
+    public static final String pot ="pot";      //6
+    public static final String aumentar ="aumentar"; //7
+    public static final String decremento ="decremento"; //8
+    
+    
+    public static final String documento="documento";
+    public static final String observador="observador";
+    public static final String obtener="obtener";
+    
+    public static final String setelem="setelem";
+    public static final String valorobjeto="valorObjeto";
+    public static final String valorchtml="valorchtml";
+    
+    public static final String valores ="valores";
+    
+    
+    
+    
+    
+    
+    
+    
+            
+}
